d0eca6b79463af7107a9d9bb9486fb815114893a
DRAGON\Dean <dfentes@gmail.com>
2018-09-13 10:18:39 +0200

    Initial Commit

---
+++ DatingApp.API/Data/DataContext.cs
@@ -0,0 +1,19 @@
+﻿using DatingApp.API.Models;
+using JetBrains.Annotations;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DatingApp.API.Data
+{
+    public class DataContext : DbContext
+    {
+        //[NotNullAttribute] 
+        public DataContext(DbContextOptions options) : base(options)  {}
+
+        public DbSet<Value> Values { get; set; }
+
+    }
+}
